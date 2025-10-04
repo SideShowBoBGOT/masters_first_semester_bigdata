78dfc26312b93681b106c005ab3228b050ee685c
account <account@mail.com>
2025-10-04 20:23:18 +0300

    loaded data

--- lab_3/src/main/scala/Main.scala
+++ lab_3/src/main/scala/Main.scala
@@ -2,5 +2,23 @@ import org.apache.{spark => spark}
 
 @main def main() =
   val sparkSession = spark.sql.SparkSession.builder().appName("local").getOrCreate()
+  import sparkSession.implicits._
+  sparkSession.read.options(Map(
+      "header" -> "true"
+    ))
+    .csv("traindata/RecGym.csv")
+    .select(
+      $"Subject" as "subj",
+      $"Position" as "pos",
+      $"Session" as "ses",
+      $"A_x" as "ax",
+      $"A_y" as "ay",
+      $"A_z" as "az",
+      $"G_x" as "gx",
+      $"G_y" as "gy",
+      $"G_z" as "gz",
+      $"C_1" as "cap",
+      $"workout" as "work"
+    )
   println("Hello World!")
   sparkSession.stop()
